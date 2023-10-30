8b0a5758441a50c031129bba9d9c0ef72a4b8701
bhop <saschamateo@gmail.com>
2023-10-30 14:59:59 +0200

    Select in which folder you'll install Rise.

--- README.md
+++ README.md
@@ -1,40 +1,17 @@
-# electron-quick-start
+## Installation of complete example app
 
-**Clone and run for a quick way to see Electron in action.**
+(For the tutorial starting point app, see the [`src-start` directory](/src-start))
 
-This is a minimal Electron application based on the [Quick Start Guide](https://electronjs.org/docs/latest/tutorial/quick-start) within the Electron documentation.
+Assuming you have [Node](https://nodejs.org/en/) installed, clone this repo and run `npm install` in the `src` directory.
 
-A basic Electron application needs just these files:
+#### Command line:
 
-- `package.json` - Points to the app's main file and lists its details and dependencies.
-- `main.js` - Starts the app and creates a browser window to render HTML. This is the app's **main process**.
-- `index.html` - A web page to render. This is the app's **renderer process**.
-- `preload.js` - A content script that runs before the renderer process loads.
+```
+git clone https://github.com/binaryfunt/electron-seamless-titlebar-tutorial.git
 
-You can learn more about each of these components in depth within the [Tutorial](https://electronjs.org/docs/latest/tutorial/tutorial-prerequisites).
+cd electron-seamless-titlebar-tutorial/src
 
-## To Use
-
-To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
-
-```bash
-# Clone this repository
-git clone https://github.com/electron/electron-quick-start
-# Go into the repository
-cd electron-quick-start
-# Install dependencies
 npm install
-# Run the app
+
 npm start
 ```
-
-Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
-
-## Resources for Learning Electron
-
-- [electronjs.org/docs](https://electronjs.org/docs) - all of Electron's documentation
-- [Electron Fiddle](https://electronjs.org/fiddle) - Electron Fiddle, an app to test small Electron experiments
-
-## License
-
-[CC0 1.0 (Public Domain)](LICENSE.md)
--- renderer.js
+++ renderer.js
@@ -1,7 +1,117 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const remote = require('electron').remote;
+
+const win = remote.getCurrentWindow(); /* Note this is different from the
+html global `window` variable */
+
+function showInstallContainer() {
+    const installContainer = document.getElementById('install-container');
+    installContainer.classList.remove('hidden');
+  }
+  
+  
+
+// When document has loaded, initialize
+document.onreadystatechange = (event) => {
+    if (document.readyState === "complete") {
+        handleWindowControls();
+
+        // Show the loader
+        const loader = document.getElementById('loader');
+        loader.style.display = 'block';
+
+        // Hide the loader after 5 seconds
+        setTimeout(() => {
+            // Add the fade-out class to initiate the fade-out effect
+            loader.classList.add('fade-out');
+
+            // Hide the loader after the transition ends
+            loader.addEventListener('transitionend', () => {
+                loader.style.display = 'none';
+            
+                // Call the showInstallContainer function here to make the install-container appear
+                showInstallContainer();
+            
+                // Create a gradual fade-in effect by changing the opacity
+                const installContainer = document.getElementById('install-container');
+                let opacity = 0;
+                const fadeInterval = setInterval(() => {
+                    opacity += 0.05; // You can adjust the increment as needed
+                    installContainer.style.opacity = opacity;
+                    if (opacity >= 1) {
+                        clearInterval(fadeInterval); // Stop the interval when opacity reaches 1
+                    }
+                }, 50); // You can adjust the interval time as needed
+            }, { once: true });
+        }, 2500);
+    }
+};
+
+// Wait for the loader to finish fading out
+
+  
+// Add a click event listener to the custom button
+// Add a click event listener to the custom button
+document.querySelector('.custom-button').addEventListener('click', function () {
+    document.getElementById('folder-selector').click();
+});
+
+// Add an event listener to handle the selected folder
+document.getElementById('folder-selector').addEventListener('change', function (e) {
+    const selectedFolder = e.target.files[0];
+    if (selectedFolder) {
+        // Handle the selected folder here
+        console.log('Selected folder path:', selectedFolder.path);
+
+        // Hide the folder selector and show the Next button
+        const folderSelectorContainer = document.getElementById('folder-selector-container');
+        const nextButtonContainer = document.getElementById('next-button-container');
+
+        folderSelectorContainer.style.display = 'none';
+        nextButtonContainer.style.display = 'block';
+    }
+});
+
+
+
+window.onbeforeunload = (event) => {
+    /* If the window is reloaded, remove win event listeners
+    (DOM element listeners get auto garbage collected but not
+    Electron win listeners as the win is not dereferenced unless closed) */
+    win.removeAllListeners();
+}
+
+function handleWindowControls() {
+    // Make minimise/maximise/restore/close buttons work when they are clicked
+    document.getElementById('min-button').addEventListener("click", event => {
+        win.minimize();
+    });
+
+    document.getElementById('max-button').addEventListener("click", event => {
+        win.maximize();
+    });
+
+    document.getElementById('restore-button').addEventListener("click", event => {
+        win.unmaximize();
+    });
+
+    document.getElementById('close-button').addEventListener("click", event => {
+        win.close();
+    });
+
+    // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
+    toggleMaxRestoreButtons();
+    win.on('maximize', toggleMaxRestoreButtons);
+    win.on('unmaximize', toggleMaxRestoreButtons);
+
+    function toggleMaxRestoreButtons() {
+        if (win.isMaximized()) {
+            document.body.classList.add('maximized');
+        } else {
+            document.body.classList.remove('maximized');
+        }
+    }
+}
+
